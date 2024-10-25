4f974bcf427d1e9546f239549fac3189148cb619
twguest <trey.guest@xfel.eu>
2024-10-25 10:29:14 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kickercontrol',
+    version='0.1.0',
+    author='Trey Guest',
+    author_email='trey.guest@xfel.eu',
+    description='A package for controlling kicker devices and performing beam steering using N-dimensional scan tools.',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'xarray',
+        ],
+    entry_points={
+        'console_scripts': [],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: European XFEL'
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
